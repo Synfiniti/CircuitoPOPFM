e14d1876025f1d1b41f806fd867e1f31e7b08cbf
Jean Gutiérrez <141754865+Synfiniti@users.noreply.github.com>
2025-07-25 12:58:42 -0400

    Update _layout.tsx

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -3,25 +3,24 @@ import { Stack } from "expo-router";
 import * as SplashScreenExpo from "expo-splash-screen";
 import SplashScreen from "./splash";
 
-// Mantén el splash screen nativo visible hasta que lo ocultemos explícitamente desde splash.jsx.
+
 SplashScreenExpo.preventAutoHideAsync();
 
 export default function Layout() {
   const [splashComponentFinished, setSplashComponentFinished] = useState(false);
 
-  // No necesitamos un useEffect para `hideAsync` aquí,
-  // ya que `splash.jsx` lo llamará directamente.
+  .
 
   const handleSplashAnimationFinished = useCallback(() => {
     setSplashComponentFinished(true);
   }, []);
 
-  // Si la animación de tu SplashScreen.jsx no ha terminado, muéstralo.
+  
   if (!splashComponentFinished) {
     return <SplashScreen onFinishLoading={handleSplashAnimationFinished} />;
   }
 
-  // Una vez que tu SplashScreen.jsx ha terminado, renderiza la pila principal.
+  
   return (
     <Stack>
       <Stack.Screen name="index" options={{ headerShown: false }} />
